a5cd4fff1db0e052d41e53bbe434ab2278925f93
Fabián Abel Sajama <sajama230@gmail.com>
2023-04-16 20:14:00 -0300

    add api, event, home

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { EventListScreen } from './src/screens/event-list/EventListScreen';
+import { HomeScreen } from './src/screens/home/HomeScreen';
+import { COLORS, SPACING } from './src/utils/theme';
 
+
+const TAB_ICON = {
+  Home: 'home',
+  Explorar: 'search'
+  //Profile: 'person'
+}
+
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name]// TAB_ICON[Home]
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: COLORS.primary,
+    tabBarInactiveTintColor: COLORS.inactive,
+    headerShown: false,
+    tabBarStyle: styles.tabBar
+
+  }
+}
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='Explorar' component={EventListScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style='auto' />
+    </>
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  tabBar: {
+    height: SPACING.xxxl,
+    paddingBottom: SPACING.xs,
+    paddingTop: SPACING.xs
+  }
+})
---
+++ src/api/api.js
@@ -0,0 +1,4 @@
+
+export const api = {
+    server: 'https://6435d3bb537112453fe040ad.mockapi.io/'
+}
