d110ddae71bee079f7a02b2302f12f57d40d95f4
Fabián Abel Sajama <sajama230@gmail.com>
2023-04-18 12:54:10 -0300

    update app.js

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//import Ionicons from 'react-native-vector-icons/Ionicons';
 import { MainStackScreen } from './src/screens/event-list/MainStackScreen';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { EventDetailScreen } from './src/screens/event-detail/EventDetailScreen';
