37e23834e3dc76e487506827d92a8828117b7e41
juanmartinaparicio <juanm_aparicio@hotmail.com>
2023-04-19 00:17:15 -0300

    Home&UserScreen

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { View, Text } from "react-native";
+
+import React from 'react'
+import { ImageBackground, SafeAreaView, Text } from 'react-native'
+import { styles } from './HomeScreen.styles'
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-};
+    <SafeAreaView style={styles.container}>
+      <ImageBackground style={styles.bgImage} source={require('../../../assets/llamaJujuy.png')}>
+        <Text style={styles.title}>
+          El sitio donde vas a encontrar todos los eventos de jujuy!!
+        </Text>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
--- src/user/UserInfoScreen.jsx
+++ src/user/UserInfoScreen.jsx
@@ -1,21 +1,46 @@
-import React, { useContext } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { styles } from "./UserInfoScreen.styles";
-import { UserContext } from "../contexts/UserContext";
+
+import React, { useContext } from 'react'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { styles } from './UserInfoScreen.styles'
+import { UserContext } from '../contexts/UserContext'
 
 export const UserInfoScreen = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser, setCurrentUser } = useContext(UserContext)
 
   const handleLogout = () => {
-    setCurrentUser(null);
-  };
+    setCurrentUser(null)
+  }
 
   return (
     <ScrollView style={styles.container}>
-      <Text>Info User</Text>
-      <TouchableOpacity style={styles.button} onPress={handleLogout}>
-        <Text style={styles.buttonText}>Salir</Text>
-      </TouchableOpacity>
+      <View style={styles.header}>
+        <Image
+          style={styles.profileImage}
+          source={require('../../assets/llamaJujuy.png')}
+        />
+        <View style={styles.profileInfo}>
+          <Text style={styles.profileName}>{currentUser.username}</Text>
+          <Text style={styles.profileLocation}>Jujuy, Argentina</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Salir</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.sectionTitle}>Sobre mi</Text>
+        <Text style={styles.sectionText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+          tellus eu nisi tincidunt ultrices. Morbi id dictum ipsum. Nunc nec
+          lacus massa. Integer eget elit non elit sodales maximus.
+        </Text>
+        <Text style={styles.sectionTitle}>Intereses</Text>
+        <Text style={styles.sectionText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+          tellus eu nisi tincidunt ultrices. Morbi id dictum ipsum. Nunc nec
+          lacus massa. Integer eget elit non elit sodales maximus.
+        </Text>
+      </View>
+
     </ScrollView>
-  );
-};
+  )
+}
